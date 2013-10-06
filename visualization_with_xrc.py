d910349d6f61f4b45ef71ca0171a2ec2db7ad7dc
nvitucci <nick_vitucci@yahoo.it>
2013-10-06 18:35:44 +0200

    Added checkboxes with properties' characteristics

--- visualization_with_xrc.py
+++ visualization_with_xrc.py
@@ -7,6 +7,20 @@ import wx.xrc as xrc
 from rdflib import RDF, RDFS, OWL
 
 class MyApp(wx.App):
+	def updateObjProp(self, ch):
+		for p in self.objPropBoxes.values():
+			p.SetValue(False)
+		
+		for c in ch:
+			self.objPropBoxes[c].SetValue(True)
+		
+	def updateDataProp(self, ch):
+		for p in self.dataPropBoxes.values():
+			p.SetValue(False)
+
+		for c in ch:
+			self.dataPropBoxes[c].SetValue(True)
+		
 	def viewMode(self, item, g, mode):
 		if mode == 'qname':
 			try:
@@ -25,19 +39,28 @@ class MyApp(wx.App):
 		root = tree.AddRoot(self.viewMode(rootName, g, mode))
 
 		for i in new_lev:
-			new_ptrs[i] = tree.AppendItem(root, self.viewMode(i, g, mode))
+			new_ptrs[i] = tree.AppendItem(root, self.viewMode(i, g, mode), data=wx.TreeItemData(i))
 
 		while len(new_lev) > 0:
 			new_lev_temp = []
 			for i in new_lev:
 				new_sub = [x[0] for x in childrenMatrix if x[1] == i]
 				for s in new_sub:
-					new_ptrs[s] = tree.AppendItem(new_ptrs[i], self.viewMode(s, g, mode))
+					new_ptrs[s] = tree.AppendItem(new_ptrs[i], self.viewMode(s, g, mode), data=wx.TreeItemData(s))
 					new_lev_temp.append(s)
 
 			new_lev = new_lev_temp
 
 	def OnInit(self):
+		self.chars = [
+			OWL['FunctionalProperty'], 
+			OWL['InverseFunctionalProperty'], 
+			OWL['TransitiveProperty'], 
+			OWL['SymmetricProperty'], 
+			OWL['AsymmetricProperty'], 
+			OWL['ReflexiveProperty'], 
+			OWL['IrreflexiveProperty']]
+		
 		xrcfile = 'gui.xrc'
 		self.xrc = xrc.XmlResource(xrcfile)
 
@@ -48,6 +71,18 @@ class MyApp(wx.App):
 		self.objPropTree = xrc.XRCCTRL(self.frame, "objPropTree")
 		self.dataPropTree = xrc.XRCCTRL(self.frame, "dataPropTree")
 
+		self.objPropBoxes = {}
+		self.objPropBoxes[self.chars[0]] = xrc.XRCCTRL(self.frame, "funcProp")
+		self.objPropBoxes[self.chars[1]] = xrc.XRCCTRL(self.frame, "invFuncProp")
+		self.objPropBoxes[self.chars[2]] = xrc.XRCCTRL(self.frame, "transProp")
+		self.objPropBoxes[self.chars[3]] = xrc.XRCCTRL(self.frame, "symProp")
+		self.objPropBoxes[self.chars[4]] = xrc.XRCCTRL(self.frame, "asymProp")
+		self.objPropBoxes[self.chars[5]] = xrc.XRCCTRL(self.frame, "refProp")
+		self.objPropBoxes[self.chars[6]] = xrc.XRCCTRL(self.frame, "irrefProp")
+
+		self.dataPropBoxes = {}
+		self.dataPropBoxes[self.chars[0]] = xrc.XRCCTRL(self.frame, "dataFuncProp")
+
 		# ----------------------------
 		
 		g = rdflib.Graph()
@@ -57,31 +92,38 @@ class MyApp(wx.App):
 		classes = set()
 		for t in g:
 		    if t[2] == OWL['Class'] and not type(t[0]) == rdflib.BNode:
-		        classes.add(t[0].toPython())
+		        classes.add(t[0])
 
 		# Set of all object properties
 		objprops = set()
 		for t in g:
-		    if t[2] == OWL['ObjectProperty'] and not type(t[0]) == rdflib.BNode:
-		        objprops.add(t[0].toPython())
+			if t[2] == OWL['ObjectProperty'] and not type(t[0]) == rdflib.BNode:
+				objprops.add(t[0])
 
 		# Set of all datatype properties
 		dataprops = set()
 		for t in g:
-		    if t[2] == OWL['DatatypeProperty'] and not type(t[0]) == rdflib.BNode:
-		        dataprops.add(t[0].toPython())
+			if t[2] == OWL['DatatypeProperty'] and not type(t[0]) == rdflib.BNode:
+				dataprops.add(t[0])
 
 		# List of pairs of (subclass, superclass)
 		subclasses = []
 		for t in g:
-		    if t[1] == RDFS['subClassOf']:
-		        subclasses.append((t[0].toPython(), t[2].toPython()))
+			if t[1] == RDFS['subClassOf']:
+				subclasses.append((t[0], t[2]))
 
 		# List of pairs of (subproperty, superproperty)
 		subprops = []
 		for t in g:
-		    if t[1] == RDFS['subPropertyOf']:
-		        subprops.append((t[0].toPython(), t[2].toPython()))
+			if t[1] == RDFS['subPropertyOf']:
+				subprops.append((t[0], t[2]))
+
+		# Characteristics of each property
+		self.prop_chars = []
+
+		for t in g:                     
+			if (t[0] in objprops or t[0] in dataprops) and t[1] == RDF['type'] and t[2] in self.chars:
+				self.prop_chars.append((t[0], t[2]))
 
 		# Top classes (classes not having a superclass)
 		new_lev_cl = classes.difference(set([x[0] for x in subclasses]))
@@ -100,11 +142,25 @@ class MyApp(wx.App):
 		
 		# wx.EVT_TREE_SEL_CHANGED(self.classTree, self.classTree.GetId(), self.OnSelChanged)
 		self.frame.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnSelChanged, id=xrc.XRCID("classTree"))
+		self.frame.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnSelPropChanged, id=xrc.XRCID("objPropTree"))
+		self.frame.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnSelDataPropChanged, id=xrc.XRCID("dataPropTree"))
 		
 		self.frame.Centre()
 		self.frame.Show(1)
 
 		return True
+
+	def OnSelPropChanged(self, event):
+		item = self.objPropTree.GetPyData(event.GetItem())
+		chars = [x[1] for x in self.prop_chars if x[0] == item]
+
+		self.updateObjProp(chars)
+
+	def OnSelDataPropChanged(self, event):
+		item = self.dataPropTree.GetPyData(event.GetItem())
+		chars = [x[1] for x in self.prop_chars if x[0] == item]
+
+		self.updateDataProp(chars)
 
 	def OnSelChanged(self, event):
 		item = event.GetItem()
